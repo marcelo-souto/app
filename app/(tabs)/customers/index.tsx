227d9619c92828572262aefcfbfbb15477fe1db6
marcelo-souto <marcelosouto676@gmail.com>
2024-11-18 20:55:31 -0300

    adiciona nova tela de clientes com listagem e navegação para cadastro e edição

---
+++ app/(tabs)/customers/index.tsx
@@ -0,0 +1,45 @@
+import {
+  CustomerList,
+  CustomerListItem,
+} from "@/components/customer-list/customer-list";
+import { ResourceHeader } from "@/components/resource/resource";
+import { Button } from "@/components/ui/button/button";
+import { Page, PageTitle } from "@/components/ui/page/page";
+import { useCustomerGetAllRequest } from "@/hooks/customer/customer-request";
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { H1, ScrollView, Text, XStack, YStack } from "tamagui";
+
+export default function Customers() {
+  const { data, isLoading, isError } = useCustomerGetAllRequest();
+
+  if (isLoading) return <Text>Loading...</Text>;
+
+  return (
+    <Page>
+      <ResourceHeader>
+        <PageTitle>Clientes</PageTitle>
+        <Button onPress={() => router.push("/customers/create")}>
+          Cadastrar
+        </Button>
+      </ResourceHeader>
+
+      <CustomerList>
+        {data?.map((customer) => (
+          <CustomerListItem
+            key={customer.id}
+            onPress={() =>
+              router.push({
+                pathname: "/customers/edit/[id]",
+                params: {
+                  id: customer.id,
+                },
+              })
+            }
+            data={customer}
+          />
+        ))}
+      </CustomerList>
+    </Page>
+  );
+}
