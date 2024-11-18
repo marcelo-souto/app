c335db552dc0f44694b02a00bb7647eb4b6cfb64
marcelo-souto <marcelosouto676@gmail.com>
2024-11-18 20:56:01 -0300

    adiciona tela de edição de produto com formulário

---
+++ app/(tabs)/products/edit/[id].tsx
@@ -0,0 +1,19 @@
+import { ProductForm } from "@/components/product-form/product-form";
+import { Page } from "@/components/ui/page/page";
+import { useProductGetByIdRequest } from "@/hooks/product/product-request";
+import { useLocalSearchParams } from "expo-router";
+import { Text } from "tamagui";
+
+export default function EditProduct() {
+  
+  const { id } = useLocalSearchParams();
+  const { data, isLoading } = useProductGetByIdRequest(id as string);
+
+  if (isLoading) return <Text>Loading...</Text>;
+
+  return (
+    <Page>
+      <ProductForm key={data?.id} data={data} mode="edit" />
+    </Page>
+  );
+}
