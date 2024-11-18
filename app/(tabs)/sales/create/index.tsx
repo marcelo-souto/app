3f4fde44719017afc9a7bbcd0018b3a7cbabb753
marcelo-souto <marcelosouto676@gmail.com>
2024-11-18 20:56:14 -0300

    adiciona tela de criação de venda

---
+++ app/(tabs)/sales/create/index.tsx
@@ -0,0 +1,10 @@
+import { Page } from "@/components/ui/page/page";
+import { Text } from "tamagui";
+
+export default function CreateSale() {
+  return (
+    <Page>
+      <Text>Create Sale</Text>
+    </Page>
+  );
+}
