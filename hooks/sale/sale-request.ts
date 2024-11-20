4ac45dfe8429ae9fa3cf48206d9dee5a4fb61674
marcelo-souto <marcelosouto676@gmail.com>
2024-11-20 17:43:25 -0300

    adiciona hook useCreateSaleRequest para gerenciamento de criação de vendas

--- hooks/sale/sale-request.ts
+++ hooks/sale/sale-request.ts
@@ -1,12 +1,31 @@
 import { saleService } from "@/services/sales/sale-service";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useSaleGetAllRequest = () => {
-  
   const query = useQuery({
     queryKey: ["get-all-sales"],
     queryFn: saleService.getAll,
   });
 
   return query;
+};
+
+export const useCreateSaleRequest = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationKey: ["create-sale"],
+    mutationFn: saleService.create,
+
+    onSuccess: (_, { customerId }) => {
+      queryClient.invalidateQueries({
+        queryKey: ["get-all-sales"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["get-customer-by-id", customerId],
+      });
+    },
+  });
+
+  return mutation;
 };
