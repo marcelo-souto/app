17599c589f39515de67ff082271b904f1f170ced
marcelo-souto <marcelosouto676@gmail.com>
2024-11-20 17:41:43 -0300

    substitui mensagem de carregamento por componente de tela de carregamento

--- app/(tabs)/customers/edit/[id].tsx
+++ app/(tabs)/customers/edit/[id].tsx
@@ -1,4 +1,5 @@
 import { CustomerForm } from "@/components/customer-form/customer-form";
+import { LoadingScreen } from "@/components/loading/loading";
 import { Page } from "@/components/ui/page/page";
 import { useCustomerGetByIdRequest } from "@/hooks/customer/customer-request";
 import { useLocalSearchParams } from "expo-router";
@@ -8,8 +9,7 @@ export default function EditCustomer() {
   const { id } = useLocalSearchParams();
   const { data, isLoading } = useCustomerGetByIdRequest(id as string);
 
-  if (isLoading) return <Text>Loading...</Text>;
-
+  if (isLoading) return <LoadingScreen />;
   return (
     <Page>
       <CustomerForm key={data?.id} data={data} mode="edit" />
