7151f8b0b5334be05347c50378ebe67f1ac44bcd
marcelo-souto <marcelosouto676@gmail.com>
2024-11-20 17:43:04 -0300

    adiciona componente Sheet com suporte a controle de abertura e fechamento

---
+++ components/ui/sheet/sheet.tsx
@@ -0,0 +1,47 @@
+import { ChevronDown } from "@tamagui/lucide-icons";
+import React, { useImperativeHandle, useState } from "react";
+import { Button, Sheet as TSheet, SheetProps as TSheetProps } from "tamagui";
+
+type SheetProps = TSheetProps;
+
+export type SheetRef = {
+  open: () => void;
+  close: () => void;
+};
+
+const Sheet = React.forwardRef<SheetRef, SheetProps>((props, ref) => {
+  const [open, setOpen] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    open: () => setOpen(true),
+    close: () => setOpen(false),
+  }));
+
+  return (
+    <TSheet
+      forceRemoveScrollEnabled={open}
+      modal={true}
+      open={open}
+      onOpenChange={setOpen}
+      snapPoints={[600, 600]}
+      snapPointsMode={"constant"}
+      zIndex={100_000}
+      dismissOnSnapToBottom={true}
+      unmountChildrenWhenHidden={true}
+      {...props}
+    >
+      <TSheet.Overlay
+        animation="lazy"
+        enterStyle={{ opacity: 0 }}
+        exitStyle={{ opacity: 0 }}
+      />
+
+      <TSheet.Handle />
+      <TSheet.Frame padding={16}>
+        {props.children}
+      </TSheet.Frame>
+    </TSheet>
+  );
+});
+
+export { Sheet };
